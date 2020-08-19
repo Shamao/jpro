89e939bb2d2ff75cf53c53e503aad0fbc6568b3c
Shamao <994516742@qq.com>
2020-08-20 01:29:35 +0800

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Integer a = 1;
+        Integer b = 1;
+
+        System.out.println(a == b);
+
+
+        Integer c = 200;
+        Integer d = 200;
+        System.out.println(c == d);
+    }
+}
